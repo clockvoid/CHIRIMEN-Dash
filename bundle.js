8a3aae14b0ce6d6630e5a7d5611b89e85ca3a51a
clockvoid <hidenari_1@yahoo.co.jp>
2017-07-09 14:01:42 +0900

    add: request behavior

--- bundle.js
+++ bundle.js
@@ -71,10 +71,25 @@ module.exports =
 const http = __webpack_require__(1);
 const $ = __webpack_require__(2);
 
+var help = function () {
+  return 'helped';
+};
+
+var check = function () {
+  return 'checked!!';
+};
+
 http.createServer((req, res) => {
-  console.log(req.headers);
+  console.log(req.url);
+  var message = req.url;
+  var response;
+  if (message.match(/help/)) {
+    response = help();
+  } else if (message.match(/check/)) {
+    response = check();
+  }
   res.writeHead(200, { 'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*' });
-  res.end('Hello World\n');
+  res.end(response);
 }).listen(1337);
 
 console.log('Server running at http://localhost:1337/');
--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const http = require('http');
 const $ = require('jquery');
 
+var help = function() {
+  return 'helped';
+}
+
+var check = function() {
+  return 'checked!!';
+}
+
 http.createServer((req, res) => {
-  console.log(req.headers);
+  console.log(req.url);
+  var message = req.url;
+  var response;
+  if (message.match(/help/)) {
+    response = help();
+  } else if (message.match(/check/)) {
+    response = check();
+  }
   res.writeHead(200, {'Content-Type': 'text/plain', 'Access-Control-Allow-Origin': '*'});
-  res.end('Hello World\n');
+  res.end(response);
 }).listen(1337);
 
 console.log('Server running at http://localhost:1337/');
